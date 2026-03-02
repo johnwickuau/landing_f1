aa3791de045b06c980ae97b9b8d8d1abe0ee06ec
jonhwickuau <joaoguitins11@gmail.com>
2026-03-02 08:47:17 -0300

    Criaçao do esqueleto da pagina

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+app.get("/fernando_alonso", (req, res, next) => {
+    return res.status(200).sendFile("alonso.html", {root: './'});
+});
+
+app.get("/charles_leclerc", (req, res, next) => {
+    return res.status(200).sendFile("leclerc.html", {root: './'});
+});
+
+app.get("/max_verstappen", (req, res, next) => {
+    return res.status(200).sendFile("max.html", {root: './'});
+});
